5d14651dcbe10c76efe6db25b20bded355f80111
Vladislav <vlad.unchuris@gmail.com>
2018-04-28 02:00:05 +0600

    now MainActivity implements response callback

--- app/src/main/java/unchuris/vladislav/gallery/activity/MainActivity.java
+++ app/src/main/java/unchuris/vladislav/gallery/activity/MainActivity.java
@@ -7,30 +7,22 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import unchuris.vladislav.gallery.R;
 import unchuris.vladislav.gallery.api.IApiGenerationLink;
+import unchuris.vladislav.gallery.api.IParsing;
+import unchuris.vladislav.gallery.api.yandex.ParsingResponse;
 import unchuris.vladislav.gallery.api.yandex.YandexDisk;
-import unchuris.vladislav.gallery.app.AppController;
 import unchuris.vladislav.gallery.model.ImageYandexDisk;
+import unchuris.vladislav.gallery.utils.Fetcher;
+import unchuris.vladislav.gallery.utils.IResponseCallback;
 
 /**
  * Activity for display the main screen, which contains an image gallery.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IResponseCallback<ImageYandexDisk>{
     /**
      * Log tag.
      */
@@ -82,52 +74,13 @@ public class MainActivity extends AppCompatActivity {
         IApiGenerationLink yd = new YandexDisk();
         pathToDownload = yd.getPublicLink(PUBLIC_FOLDER_URL, "preview_size=M&preview_crop=true");
 
-        fetchImages();
+        final IParsing<ImageYandexDisk> parsing = new ParsingResponse();
+        Fetcher<ImageYandexDisk> iyd = new Fetcher<>(this);
+        iyd.fetchImages(pathToDownload, parsing);
     }
 
-    private void fetchImages() {
-        final DateTimeFormatter DATE_FORMAT = ISODateTimeFormat.dateTimeNoMillis();
-        pDialog.setMessage("Downloading images...");
-        pDialog.show();
-        StringRequest req = new StringRequest(pathToDownload,
-                new Response.Listener<String>() {
-            @Override
-            public void onResponse(final String response) {
-                Log.d(TAG, response);
-
-                pDialog.hide();
-
-                images.clear();
-                try {
-                    JSONObject responseObj = new JSONObject(response);
-                    JSONObject embedded = responseObj.getJSONObject("_embedded");
-                    JSONArray returnedData = embedded.getJSONArray("items");
-
-                    for (int i = 0; i < returnedData.length(); i++) {
-                        JSONObject object = returnedData.getJSONObject(i);
-                        ImageYandexDisk image = new ImageYandexDisk();
-                        image.setName(object.getString("name"));
-                        image.setPreview(object.getString("preview"));
-                        image.setSize(object.getInt("size"));
-                        image.setDateTimeCreated(
-                                DATE_FORMAT.parseDateTime(object.getString("created")));
-                        image.setDateTimeModified(
-                                DATE_FORMAT.parseDateTime(object.getString("modified")));
-                        images.add(image);
-                    }
-                } catch (JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-                }
-                //mAdapter.notifyDataSetChanged();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(final VolleyError error) {
-                Log.e(TAG, "Error: " + error.getMessage());
-                pDialog.hide();
-            }
-        });
-
-        AppController.getInstance().addToRequestQueue(req);
+    @Override
+    public void response(ArrayList<ImageYandexDisk> response) {
+        //TO DO
     }
 }
