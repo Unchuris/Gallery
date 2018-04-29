9e07329351958990ba81d430658075e48b2fe482
Vladislav <vlad.unchuris@gmail.com>
2018-04-30 01:39:26 +0600

    Added state saving and the number of columns now depends orientation of the device.

--- app/src/main/java/unchuris/vladislav/gallery/activity/MainActivity.java
+++ app/src/main/java/unchuris/vladislav/gallery/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package unchuris.vladislav.gallery.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -7,21 +8,22 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import java.util.ArrayList;
 
 import unchuris.vladislav.gallery.R;
 import unchuris.vladislav.gallery.adapter.GalleryAdapter;
-import unchuris.vladislav.gallery.utils.IClickListener;
-import unchuris.vladislav.gallery.utils.RecyclerTouchListener;
 import unchuris.vladislav.gallery.api.IApiGenerationLink;
 import unchuris.vladislav.gallery.api.IParsing;
 import unchuris.vladislav.gallery.api.yandex.ParsingResponse;
 import unchuris.vladislav.gallery.api.yandex.YandexDisk;
 import unchuris.vladislav.gallery.model.ImageYandexDisk;
 import unchuris.vladislav.gallery.utils.Fetcher;
+import unchuris.vladislav.gallery.utils.IClickListener;
 import unchuris.vladislav.gallery.utils.IResponseCallback;
+import unchuris.vladislav.gallery.utils.RecyclerTouchListener;
 
 /**
  * Activity for display the main screen, which contains an image gallery.
@@ -33,9 +35,24 @@ public class MainActivity extends AppCompatActivity implements IResponseCallback
     private static final String PUBLIC_FOLDER_URL = "https://yadi.sk/d/2juJHwM13UjXGw";
 
     /**
+     * Number of columns to display the images on the orientation portrait.
+     */
+    private static final Integer SPAN_COUNT_PORTRAIT = 2;
+
+    /**
+     * Number of columns to display the images on the orientation landscape.
+     */
+    private static final Integer SPAN_COUNT_LANDSCAPE = 2 * SPAN_COUNT_PORTRAIT;
+
+    /**
      * Number of columns to display the images.
      */
-    private static final Integer SPAN_COUNT = 2;
+    private Integer spanCount = SPAN_COUNT_PORTRAIT;
+
+    /**
+     * key for accessing an array.
+     */
+    public static final String IMAGES_RESOURCES = "images";
 
     /**
      * Instance the recyclerView.
@@ -47,6 +64,9 @@ public class MainActivity extends AppCompatActivity implements IResponseCallback
      */
     private String pathToDownload;
 
+    /**
+     * An array from the images.
+     */
     private ArrayList<ImageYandexDisk> images = new ArrayList<>();
 
     @Override
@@ -57,22 +77,81 @@ public class MainActivity extends AppCompatActivity implements IResponseCallback
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        recyclerView = findViewById(R.id.recycler_view);
+        initDisplayParams();
 
+        recyclerView = findViewById(R.id.recycler_view);
+        initRecycleView();
+
+        if (savedInstanceState != null) {
+            images = savedInstanceState.getParcelableArrayList(IMAGES_RESOURCES);
+        }
+        init(images);
+
+        addOnItemTouchListener();
+    }
+
+    private void initDisplayParams() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        Integer mImageWidth = displayMetrics.widthPixels;
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = SPAN_COUNT_LANDSCAPE;
+        }
+    }
+
+    /**
+     * InitRecycleView.
+     */
+    private void initRecycleView() {
         RecyclerView.LayoutManager mLayoutManager =
-                new GridLayoutManager(getApplicationContext(), SPAN_COUNT);
+                new GridLayoutManager(getApplicationContext(), spanCount);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
 
+    /**
+     * RecyclerView set adapter.
+     * @param img array from the images.
+     */
+    private void init(final ArrayList<ImageYandexDisk> img) {
+        if (img != null && img.isEmpty()) {
+            download();
+        } else {
+            recyclerView.setAdapter(
+                    new GalleryAdapter(getApplicationContext(), getImagesURL(img))
+            );
+        }
+    }
+
+    /**
+     * Get imagesURL
+     * @param img array from the images.
+     * @return list.
+     */
+    private ArrayList<String> getImagesURL(final ArrayList<ImageYandexDisk> img) {
+        if (img == null) {
+            return new ArrayList<>();
+        }
+        ArrayList<String> imagesURL = new ArrayList<>();
+
+        for (ImageYandexDisk item : img) {
+            imagesURL.add(item.getPreview());
+        }
+
+        return imagesURL;
+    }
+
+    /**
+     * Download the list of images.
+     */
+    private void download() {
         IApiGenerationLink yd = new YandexDisk();
-
         pathToDownload = yd.getPublicLink(PUBLIC_FOLDER_URL,
                 "preview_size=M&preview_crop=true&limit=200");
         IParsing<ImageYandexDisk> parsing = new ParsingResponse();
         Fetcher<ImageYandexDisk> iyd = new Fetcher<>(this, this);
+        // When the download finishes calling "response".
         iyd.fetchImages(pathToDownload, parsing);
-
-        addOnItemTouchListener();
     }
 
     /**
@@ -82,12 +161,9 @@ public class MainActivity extends AppCompatActivity implements IResponseCallback
     @Override
     public void response(final ArrayList<ImageYandexDisk> response) {
         images = response;
-        ArrayList<String> imagesURL = new ArrayList<>();
-        for (ImageYandexDisk item : response) {
-            imagesURL.add(item.getPreview());
-        }
-        GalleryAdapter adapter = new GalleryAdapter(getApplicationContext(), imagesURL);
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(
+                new GalleryAdapter(getApplicationContext(), getImagesURL(response))
+        );
     }
 
     /**
@@ -104,7 +180,7 @@ public class MainActivity extends AppCompatActivity implements IResponseCallback
             @Override
             public void onClick(final View view, final int position) {
                 Bundle bundle = new Bundle();
-                bundle.putSerializable("images", images);
+                bundle.putSerializable(IMAGES_RESOURCES, images);
                 bundle.putInt("position", position);
 
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -123,5 +199,15 @@ public class MainActivity extends AppCompatActivity implements IResponseCallback
 
             }
         }));
+    }
+
+    /**
+     * On save instance state.
+     * @param outState bundle.
+     */
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(IMAGES_RESOURCES, images);
     }
 }
